6ffec9061f3b6365b40b6655d65207bc9e684f51
Michael DeCuire <michaeldecuire18@gmail.com>
2025-03-01 15:23:47 -0600

    Backend up and working with Flask.

--- backend.py
+++ backend.py
@@ -1,3 +1,23 @@
+from flask import Flask
+app = Flask(__name__)
+
+# A decorator used to tell the application
+# which URL is associated function
+
+@app.route('/hello')
+def hello_world():
+    return 'hello world!'
+app.add_url_rule('/', 'hello', hello_world)
+
+
+app.debug = True
+app.run()
+app.run(debug = True)
+
+
+if __name__ == '__main__':
+    app.run()
+
 def main():
 
     print("Hello world! ")
