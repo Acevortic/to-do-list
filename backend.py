25f079f1b32a667b9f4fb054bda4a34055b14847
Michael DeCuire <michaeldecuire18@gmail.com>
2025-03-01 21:07:45 -0600

    List database for the initial backend testing

--- backend.py
+++ backend.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-# A decorator used to tell the application
-# which URL is associated function
+todos = ["Dishes", "Laundry", "Shopping"]
 
 @app.route('/hello')
 def hello_world():
     return 'hello world!'
 app.add_url_rule('/', 'hello', hello_world)
 
+@app.route('/todos', methods=["GET", "POST"]) # Get method to display all todos currently and Add a todo to the to do list
+def get_todos():
+    if request.method == 'POST':
+        todo = request.json.get('todo')
+        todos.append(todo)
+        return jsonify({'todos': todos}), 201
+    else:
+        return jsonify({'todos': todos}), 201
+
+@app.route('/todos/id') # Put a todo / update a current todo
+def update_todos():
+    return todos
+
+@app.route('/todos/id')
+def delete_todo():
+    return todos
 
 app.debug = True
 app.run()
@@ -17,7 +32,3 @@ app.run(debug = True)
 
 if __name__ == '__main__':
     app.run()
-
-def main():
-
-    print("Hello world! ")
